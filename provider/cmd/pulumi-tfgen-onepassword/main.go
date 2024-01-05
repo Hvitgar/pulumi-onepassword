d8982af6ca62cceb8baacb54220c95ab228a97d4
Benjamin Mitzkus <b.mitzkus@gmx.de>
2024-01-05 16:58:30 +0100

    ran initial project setup

--- provider/cmd/pulumi-tfgen-onepassword/main.go
+++ provider/cmd/pulumi-tfgen-onepassword/main.go
@@ -16,11 +16,11 @@ package main
 
 import (
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfgen"
-	xyz "github.com/pulumi/pulumi-xyz/provider"
-	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
+	onepassword "github.com/pulumi/pulumi-onepassword/provider"
+	"github.com/pulumi/pulumi-onepassword/provider/pkg/version"
 )
 
 func main() {
 	// Modify the path to point to the new provider
-	tfgen.Main("xyz", version.Version, xyz.Provider())
+	tfgen.Main("onepassword", version.Version, onepassword.Provider())
 }
